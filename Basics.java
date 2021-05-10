e12a70ca0129658afe3f9d12597a4815abf259a9
Hanumanth <bandisoft@gmail.com>
2021-05-10 07:58:45 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,19 @@
+public class Basics {
+
+    public static void main(String[] args) {
+
+        // logical operator
+        int empSalary=90800;
+        int empAge=45;
+        float empHeight=6.1F;
+
+        if((empSalary>100000 && empAge>40) || empHeight>6 ){
+            System.out.println("ur criteria is met");
+        }
+
+        //empSalary>100000 && empAge>40
+        
+
+    }
+
+}
